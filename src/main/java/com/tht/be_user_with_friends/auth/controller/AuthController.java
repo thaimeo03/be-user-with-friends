3bf184132a8d34fbe8f672f5fafb1f7c3bf9f82c
thaimeo03 <thaitran15072003@gmail.com>
2026-03-13 11:34:21 +0700

    add: login, logout, authentication features

--- src/main/java/com/tht/be_user_with_friends/auth/controller/AuthController.java
+++ src/main/java/com/tht/be_user_with_friends/auth/controller/AuthController.java
@@ -1,16 +1,18 @@
 package com.tht.be_user_with_friends.auth.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tht.be_user_with_friends.auth.dto.request.LoginRequest;
 import com.tht.be_user_with_friends.auth.dto.request.RegisterRequest;
+import com.tht.be_user_with_friends.auth.service.AuthService;
 import com.tht.be_user_with_friends.common.dto.response.BaseResponse;
 import com.tht.be_user_with_friends.user.service.UserService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
   private final UserService userService;
-  private final AuthenticationManager authenticationManager;
+  private final AuthService authService;
 
   @PostMapping("/register")
   public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
@@ -26,6 +28,20 @@ public class AuthController {
 
     return ResponseEntity.ok(
         BaseResponse.success("User registered successfully"));
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<?> login(@RequestBody LoginRequest request, HttpServletRequest httpRequest) throws Exception {
+    authService.login(request, httpRequest);
+
+    return ResponseEntity.ok(BaseResponse.success("Login successful"));
+  }
+
+  @PostMapping("/logout")
+  public ResponseEntity<?> logout(HttpServletRequest httpRequest) {
+    authService.logout(httpRequest);
+
+    return ResponseEntity.ok(BaseResponse.success("Logout successful"));
   }
 
 }
---
+++ src/main/java/com/tht/be_user_with_friends/auth/service/impl/AuthServiceImpl.java
@@ -0,0 +1,69 @@
+package com.tht.be_user_with_friends.auth.service.impl;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.stereotype.Service;
+
+import com.tht.be_user_with_friends.auth.dto.request.LoginRequest;
+import com.tht.be_user_with_friends.auth.service.AuthService;
+import com.tht.be_user_with_friends.common.enums.UserRole;
+import com.tht.be_user_with_friends.common.exception.BannedUserException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+  private final AuthenticationManager authenticationManager;
+
+  @Override
+  public void login(LoginRequest request, HttpServletRequest httpRequest) throws Exception {
+    try {
+      // Create authentication
+      AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.getEmail(),
+          request.getPassword());
+      Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+      // Check if user is banned
+      UserRole userRole = authentication.getAuthorities().stream()
+          .filter(auth -> auth.getAuthority().startsWith("ROLE_"))
+          .map(auth -> UserRole.valueOf(auth.getAuthority().substring(5)))
+          .findFirst()
+          .orElseThrow(() -> new BadRequestException());
+      if (userRole == UserRole.BANNED) {
+        throw new BannedUserException();
+      }
+
+      // Set authentication to security context
+      SecurityContext context = SecurityContextHolder.createEmptyContext();
+      context.setAuthentication(authentication);
+
+      // Create session and set security context
+      HttpSession session = httpRequest.getSession(true);
+      session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, context);
+    } catch (Exception e) {
+      if (e instanceof BannedUserException) {
+        throw e;
+      }
+
+      throw new BadRequestException("Invalid email or password");
+    }
+  }
+
+  @Override
+  public void logout(HttpServletRequest httpRequest) {
+    HttpSession session = httpRequest.getSession(false);
+    if (session != null) {
+      session.invalidate();
+    }
+    SecurityContextHolder.clearContext();
+  }
+}
